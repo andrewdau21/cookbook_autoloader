7ad3bdf4e12bfc702240db3c3e86c33dc57cc71d
andrewdau21 <44346254+andrewdau21@users.noreply.github.com>
2022-11-22 21:15:05 -0500

    Update autoloader.py
    initial chrome driver setup...need to update readme with instructions on getting the driver

--- autoloader.py
+++ autoloader.py
@@ -1,1 +1,13 @@
 #initial commit file
+
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(executable_path = "C:/Users/Andrew/Documents/selenium_driver/chromedriver.exe",chrome_options = chrome_options )
+
+url = "https://createmycookbook.com/projects/VjM4tW5Ey?default_email=andrew_dau%40hotmail.com"
+
+driver.get(url)
